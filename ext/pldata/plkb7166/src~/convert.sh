9d35a63b6327d85f75b93ec36045689d1b3ff29c
logicmoo <logicmoo@gmail.com>
2020-02-10 05:02:54 -0800

    unlink files

--- ext/pldata/plkb7166/src~/convert.sh
+++
@@ -1,4 +0,0 @@
-
-#find . -type f -name *.pl -exec sed -i.bak "s/a7166_//g" {} \;
-#find . -type f -name "*.pl" -exec sed -i.bak -e  's/^/ /;:a' -e 's/\([^[:alnum:]]\)\([[:digit:]]\{1,\}\)(/\1a\2(/g;ta' -e 's/^ //' {} \;
-
