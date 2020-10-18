238381b8bb22269ca487d170814d422e5834746d
Douglas R. Miles <logicmoo@gmail.com>
2020-10-18 05:15:11 -0700

    Making Logicmoo_nlu the master of pulling in deps

--- pack.pl
+++ pack.pl
@@ -6,6 +6,24 @@ packager( 'TeamSPoon/LogicMoo', 'https://github.com/TeamSPoon/' ).
 maintainer( 'TeamSPoon', 'https://github.com/TeamSPoon/' ).
 home( 'https://github.com/TeamSPoon/logicmoo_nlu' ).
 download( 'https://github.com/TeamSPoon/logicmoo_nlu/release/*.zip' ).
-requires(logicmoo_base).
-requires(body_reordering).
+requires(predicate_streams).
+requires(gvar_syntax).
+requires(logicmoo_utils).
 requires(dictoo).
+requires(multimodal_dcg).
+requires(body_reordering).
+requires(logicmoo_cg).
+requires(logicmoo_ec).
+requires(logicmoo_strorytron).
+requires(lps_corner).
+requires(eggdrop).
+requires(narsese).
+requires(pfc).
+requires(wam_common_lisp).
+requires(logicmoo_base).
+requires(prologmud).
+requires(prologmud_samples).
+requires(logicmoo_webui).
+requires(body_reordering).
+requires(multimodal_dcg).
+
